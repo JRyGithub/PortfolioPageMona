ce4e08b8f6a19298bd397082451a54ef32256f2a
JRyGithub <jryland@protonmail.com>
2021-03-01 20:59:21 +1300

    pdfAdded

--- src/Components/InformationSection/InformationSection.js
+++ src/Components/InformationSection/InformationSection.js
@@ -1,29 +1,13 @@
-
 import * as React from "react";
-import './InformationSection.scss'
+import './InformationSection.scss';
+import '../PictureRubric/PictureRubric';
+import PictureRubric from "../PictureRubric/PictureRubric";
 
 function InformationSection() {
     return (
         <article class="cf">
             <div class="fr w-25 bg-light-gray tc vh-100">
-                <div className="vh-25 flex">
-                    <div className="bgaqua h-100 w-40 dim flowers"></div>
-                    <div className="bgred h-100 w-60 dim colorPainting"></div>
-                </div>
-                <div className="vh-50 flex dib w-100">
-                    <div className="w-100">
-                        <div className="bgblue w-100 h-50 dim basketball"></div>
-                        <div className="bggreen w-100 h-50 dim phonePic"></div>
-                    </div>
-                    <div className="w-100">
-                        <div className="bgpink w-100 h-75 dim greenFlower"></div>
-                        <div className="bgsilver w-100 h-25 dim painting"></div>  
-                    </div>                 
-                </div>
-                <div className="vh-25 flex">
-                    <div className="bgblack w-60 dim houseAndSky"></div>
-                    <div className="bgyellow w-40 dim bagOnHead"></div>
-                </div>           
+                <PictureRubric />          
             </div>
             <div class="fr w-25 bg-light-gray tc vh-100">
                 <div class="fr pa3 pa4-ns bg-white black-70 measure-narrow f3 times vh-100">
---
+++ src/Components/PictureRubric/PictureRubric.js
@@ -0,0 +1,27 @@
+import * as React from "react";
+
+function PictureRubric() {
+  return (
+    <>
+    <div className="vh-25 flex">
+      <div className="bgaqua h-100 w-40 dim flowers"></div>
+      <div className="bgred h-100 w-60 dim colorPainting"></div>
+    </div>
+    <div className="vh-50 flex dib w-100">
+      <div className="w-100">
+        <div className="bgblue w-100 h-50 dim basketball"></div>
+        <div className="bggreen w-100 h-50 dim phonePic"></div>
+      </div>
+      <div className="w-100">
+        <div className="bgpink w-100 h-75 dim greenFlower"></div>
+        <div className="bgsilver w-100 h-25 dim painting"></div>
+      </div>
+    </div>
+    <div className="vh-25 flex">
+      <div className="bgblack w-60 dim houseAndSky"></div>
+      <div className="bgyellow w-40 dim bagOnHead"></div>
+    </div>
+  </>
+  )
+}
+export default PictureRubric;
