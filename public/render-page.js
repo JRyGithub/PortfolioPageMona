90d2c83bfd384b8040016b64b4c47a7ecee60b4a
Josh Ryland <jryland@protonmail.com>
2021-03-04 19:14:37 +1300

    config stuff

--- public/render-page.js
+++ public/render-page.js
@@ -1,12 +1,12 @@
 (function webpackUniversalModuleDefinition(root, factory) {
 	if(typeof exports === 'object' && typeof module === 'object')
-		module.exports = factory(require("/Users/loqgar/Desktop/Projects/PortfolioPageMona/node_modules/lodash/merge.js"), require("react"), require("react-dom/server"));
+		module.exports = factory(require("C:\\Users\\Gorilla Rig\\Desktop\\Projects\\PortfolioPageMona\\node_modules\\lodash\\merge.js"), require("react"), require("react-dom/server"));
 	else if(typeof define === 'function' && define.amd)
-		define("lib", ["/Users/loqgar/Desktop/Projects/PortfolioPageMona/node_modules/lodash/merge.js", "react", "react-dom/server"], factory);
+		define("lib", ["C:\\Users\\Gorilla Rig\\Desktop\\Projects\\PortfolioPageMona\\node_modules\\lodash\\merge.js", "react", "react-dom/server"], factory);
 	else if(typeof exports === 'object')
-		exports["lib"] = factory(require("/Users/loqgar/Desktop/Projects/PortfolioPageMona/node_modules/lodash/merge.js"), require("react"), require("react-dom/server"));
+		exports["lib"] = factory(require("C:\\Users\\Gorilla Rig\\Desktop\\Projects\\PortfolioPageMona\\node_modules\\lodash\\merge.js"), require("react"), require("react-dom/server"));
 	else
-		root["lib"] = factory(root["/Users/loqgar/Desktop/Projects/PortfolioPageMona/node_modules/lodash/merge.js"], root["react"], root["react-dom/server"]);
+		root["lib"] = factory(root["C:\\Users\\Gorilla Rig\\Desktop\\Projects\\PortfolioPageMona\\node_modules\\lodash\\merge.js"], root["react"], root["react-dom/server"]);
 })(this, function(__WEBPACK_EXTERNAL_MODULE_lodash_merge__, __WEBPACK_EXTERNAL_MODULE_react__, __WEBPACK_EXTERNAL_MODULE_react_dom_server__) {
 return /******/ (function(modules) { // webpackBootstrap
 /******/ 	// The module cache
@@ -1635,9 +1635,9 @@ if (false) {} else {
 /***/ }),
 
 /***/ "lodash/merge":
-/*!************************************************************************************************!*\
-  !*** external "/Users/loqgar/Desktop/Projects/PortfolioPageMona/node_modules/lodash/merge.js" ***!
-  \************************************************************************************************/
+/*!***************************************************************************************************************!*\
+  !*** external "C:\\Users\\Gorilla Rig\\Desktop\\Projects\\PortfolioPageMona\\node_modules\\lodash\\merge.js" ***!
+  \***************************************************************************************************************/
 /*! no static exports found */
 /***/ (function(module, exports) {
 
