4615e11c81d17cb15d2e9ec4c10d767c4f6b3467
Mona TA H <98896359+momotah@users.noreply.github.com>
2022-02-03 23:14:44 +0100

    Update InformationSection.js

--- src/Components/InformationSection/InformationSection.js
+++ src/Components/InformationSection/InformationSection.js
@@ -10,7 +10,7 @@ function InformationSection( {setRoute}) {
                 <PictureRubric  setRoute={setRoute}/>          
             </div>
             <div className="fr w-25 bg-light-gray tc vh-100">
-                <div className="fr pa3 pa4-ns bg-white black-70 measure-narrow f3 times vh-100">
+                <div className="fr pa2 pa4-ns bg-white black-70 measure-narrow f2 times vh-100">
                     <header className="bb b--black-70 pv1">
                         <h3 className="titleFont fw7 ttu tracked lh-title mt0 mb3 avenir">Mona Tamara Anaïs Harnett</h3>
                         <h4 className="titleFont fw4 i lh-title mt0">Curious. Globetrotter. Economist.</h4>
